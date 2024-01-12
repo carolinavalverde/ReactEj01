e2d3346e476cc6e6eaa3822c4f874301b3c19c30
Caro Valverde <carolina.valverde255@gmail.com>
2024-01-12 14:00:12 -0300

    agrego codigo que funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import HelloWorld from "./components/helloWorld";
 
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
